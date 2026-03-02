511b756048af4a350b76895d3392c50a75f0b530
Israel Torrico Castro <israetor@car15s4.42barcelona.com>
2026-03-02 20:14:51 +0100

    avanzado un poco el mod3

---
+++ mod3/ClapTrap.cpp
@@ -0,0 +1,20 @@
+#include "ClapTrap.h"
+
+
+ClapTrap::ClapTrap()
+{
+
+}
+ClapTrap::ClapTrap(const ClapTrap &to_copy)
+{
+
+}
+ClapTrap &ClapTrap::operator=(const ClapTrap &original)
+{
+    
+}
+ClapTrap::~ClapTrap()
+{
+
+
+}
---
+++ mod3/ClapTrap.h
@@ -0,0 +1,29 @@
+#ifndef CLAPTRAP_H
+#define CLAPTRAP_H
+
+#pragma once
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+    public:
+
+        // ortodox canonical form  {
+        ClapTrap();
+        ClapTrap(const ClapTrap &to_copy);
+        ClapTrap &operator=(const ClapTrap &original);
+        ~ClapTrap();
+        //}
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRapaired(unsigned int amount);
+        
+    private:
+    std::string Name;
+    int hitPoints;
+    int energyPoints;
+    int attackDamage;
+
+};
+#endif
