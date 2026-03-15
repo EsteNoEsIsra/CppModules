93187281a493c0b6db6c4997375d1c04b76e087b
Israel <israeltf1219@gmail.com>
2026-03-15 23:57:27 +0100

    done 00

---
+++ mod5/ex00/Bureaucrat.cpp
@@ -0,0 +1,73 @@
+ #include "Bureaucrat.h"
+
+Bureaucrat::Bureaucrat(): name("default")
+{
+    //std::cout << "Bureaucrat Default Constructor Called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string const _name, int _grade) : name(_name)
+{
+    //std::cout << "Bureaucrat Constructor Called" << std::endl;
+    if (_grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    if (_grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+    this->grade = _grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &to_copy) : name(to_copy.name), grade(to_copy.grade)
+{
+    //std::cout << "Bureaucrat Constructor Called" << std::endl;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat &original)
+{
+    if(this != &original)
+    {        
+        this->grade = original.grade;
+    }
+    //std::cout << "Bureaucrat operator Called" << std::endl;
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    //std::cout << "Bureaucrat Destructor Called" << std::endl;
+}
+
+int Bureaucrat::getGrade() const { return this->grade; }
+std::string Bureaucrat::getName() const { return this->name; }
+
+void Bureaucrat::incrementGrade()
+{
+    if ((this->grade -1) < 1)
+        throw Bureaucrat::GradeTooHighException();
+    else 
+        this->grade--;
+}   
+
+void Bureaucrat::decrementGrade()
+{
+    if ((this->grade + 1) > 150)
+        throw Bureaucrat::GradeTooLowException();
+    else 
+        this->grade++;
+}
+
+const char * Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade is too high maximun 1";
+}
+
+
+const char * Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade is too low minimun 150";
+}
+
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &obj)
+{
+    os << obj.getName() <<", Bureaucrat grade " << obj.getGrade(); 
+    return os;
+}
--- mod5/ex00/Bureaucrat.h
+++ mod5/ex00/Bureaucrat.h
@@ -1,21 +1,47 @@
-#pragma once 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+#   define RED      "\033[31m"
+#   define RESET    "\033[0m"
 
-#include <iostream>
-#include <string>
+# include <iostream>
+# include <string>
+# include <exception>
+
+
 
 class Bureaucrat
 {
     private:
         const std::string name;
-        unsigned int grade;
+        int grade;
     public:
 
     Bureaucrat();
+    Bureaucrat(std::string const _name, int _grade);
     Bureaucrat(const Bureaucrat &to_copy);
     Bureaucrat &operator=(const Bureaucrat &original);
     ~Bureaucrat();    
 
-
     int getGrade()const ;
-    std::string getName();
+    std::string getName()const;
+
+    void incrementGrade();
+    void decrementGrade();
+
+    class GradeTooHighException : public std::exception
+    {
+        public:
+            virtual const char *what() const throw ();
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+        public:
+            virtual const char *what() const throw ();
+    };
+
 };
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &obj);
+
+#endif
