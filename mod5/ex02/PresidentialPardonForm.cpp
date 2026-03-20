3d61ecef281d9e0f711b62f71f4c9cc8fa6056fe
Israel <israeltf1219@gmail.com>
2026-03-20 16:23:17 +0100

    1 line :D

--- mod5/ex02/PresidentialPardonForm.cpp
+++ mod5/ex02/PresidentialPardonForm.cpp
@@ -1,9 +1,22 @@
 #include "PresidentialPardonForm.h"
 
-PresidentialPardonForm::PresidentialPardonForm()
+PresidentialPardonForm()
 {
-}
 
-PresidentialPardonForm::~PresidentialPardonForm()
+}
+PresidentialPardonForm(const PresidentialPardonForm &to_copy)
 {
+
+}
+PresidentialPardonForm &operator=(const PresidentialPardonForm &original)
+{
+    if ( this != original)
+    {
+        this = original;
+    }
+    return *this
+}
+~PresidentialPardonForm()
+{
+
 }
