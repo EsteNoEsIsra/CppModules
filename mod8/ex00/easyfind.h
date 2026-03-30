ea2f5f3656b436b43e7fe0357ee1ae06004304d4
Israel <israeltf1219@gmail.com>
2026-03-31 01:51:02 +0200

    done

---
+++ mod8/ex00/easyfind.h
@@ -0,0 +1,30 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include<algorithm>
+# include<exception>
+
+
+class NotFoundExeption : public std::exception
+{
+	public:
+		virtual const char* what() const throw()
+		{
+			return ("Number not found in the container");
+		}
+	
+};
+
+template <typename T>
+typename T::iterator easyfind(T &container, int n)
+{
+	typename T::iterator it;
+
+	it = std::find(container.begin(), container.end(), n);
+	if (it == container.end())
+		throw NotFoundExeption();
+	return (it);
+}
+
+
+#endif
--- mod8/ex00/main.cpp
+++ mod8/ex00/main.cpp
@@ -1,9 +1,30 @@
 
+
+#include "easyfind.h"
 #include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
 
-int main()
+#define NUMBER_SIZE 10
+
+int main( void )
 {
-
+    std::vector<int> v_container;
+    for (int i = 0; i < NUMBER_SIZE ; i++)
+    {
+        v_container.push_back(i*2);
+    }
+    try
+    {
+        std::cout <<  *easyfind(v_container,2) << " --> Number found" <<std::endl;
+    }
+    catch(std::exception& e)
+    {
+        std::cerr << "Error: " <<e.what() << std::endl;
+    }
+    
+    
 
     
 
